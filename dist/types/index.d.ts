a2b41a038d9a039e2d0cf2f7914a6849417df4f7
noiissyboy <vigneshwaran.india@gmail.com>
2021-07-06 19:03:20 +0530

    Initial working copy

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,2 +1,1 @@
-import OttomanStore from './lib/OttomanStore';
-export = OttomanStore;
+export * from './lib/OttomanStore';
--- dist/types/lib/OttomanStore.d.ts
+++ dist/types/lib/OttomanStore.d.ts
@@ -1,29 +1,3 @@
 import session from 'express-session';
-import { Ottoman } from 'ottoman';
-export default class OttomanStore extends session.Store {
-    client: connectOptions;
-    collectionName: string;
-    maxExpiry: number;
-    prefix: string;
-    constructor(options: OttomanStoreOptions);
-    connectToOttoman(): {
-        ottoman: Ottoman;
-        SessionModel: import("ottoman").ModelTypes<any, any>;
-    };
-    get(sid: string, callback: (err: any, session?: session.SessionData | null) => void): void;
-    set(sid: string, session: session.SessionData, callback?: (err: any) => void): void;
-    destroy(sid: string, callback?: (err: any) => void): void;
-}
-interface OttomanStoreOptions {
-    client?: connectOptions;
-    collectionName?: string;
-    maxExpiry?: number;
-    prefix?: string;
-}
-interface connectOptions {
-    connectionString: string;
-    bucketName: string;
-    username: string;
-    password: string;
-}
-export {};
+declare const _default: (_session: typeof session) => any;
+export default _default;
